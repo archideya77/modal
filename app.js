326806cb29945030370b70dae7cec2ed13399ad2
archideya77 <archideya2002@gmail.com>
2022-06-29 23:22:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const showModal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+showModal.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
